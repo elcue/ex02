61ac0de2d905c5c6fb5f3543dfebedc09cd49d75
kimelcue-PC\kimelcue <kimelcue@gmail.com>
2017-02-24 16:01:04 +0900

    interceptor test

---
+++ src/main/java/org/zerock/interceptor/SampleInterceptor.java
@@ -0,0 +1,48 @@
+package org.zerock.interceptor;
+
+import java.lang.reflect.Method;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+/**
+ * SpringBoard 
+ * 클래스명 : SampleInterceptor.java
+ * 작성자 : elcue
+ * 작성일 : 2017. 2. 24.
+ * 클래스 소개 : 인터셉터 사용
+ */
+public class SampleInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception{
+		System.out.println("pre handle.......................");
+		
+		HandlerMethod method = (HandlerMethod) handler;
+		Method methodObj = method.getMethod();
+		
+		System.out.println("Bean : " + method.getBean());
+		System.out.println("Method : " + methodObj);
+		
+		return true;
+	}
+
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception{
+		System.out.println("post handle......................");
+		
+		Object result=modelAndView.getModel().get("result");
+		
+		if(result != null){
+			request.getSession().setAttribute("result", result);
+			response.sendRedirect("/doA");
+		}
+	}
+	
+
+}
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,5 +1,4 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 
 <%@ include file="include/header.jsp" %>
@@ -14,6 +13,7 @@
 				<div class="box">
 					<div class="box-header with-border">
 						<h3 class="box-title">HOME PAGE</h3>
+						<h2>${result }</h2>
 					</div>
 				</div>
 			</div>
